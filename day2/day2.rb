829c8fa0107eca1fb684097d7b4bbf297a05e65f
Jason Loo <loo.jason95@gmail.com>
2015-12-03 01:50:12 -0500

    Day 2 Part 2

--- day2/day2.rb
+++ day2/day2.rb
@@ -10,28 +10,51 @@
 # A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
 # All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
 
+# --- Part Two ---
+
+# The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, which they would again like to be exact.
+
+# The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+
+# For example:
+
+# A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+# A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+# How many total feet of ribbon should they order?
+
+
 module Advent
   class Day2
 
     def initialize
-      @file = File.open(File.dirname(__FILE__) + "/input.txt", "r")
+      @boxes = File.readlines(File.dirname(__FILE__) + "/input.txt")
     end
 
     def problem1
-      total_wrapping_paper = 0
-      while line = @file.gets
-        dimensions = line.split("x")
-        total_wrapping_paper += wrapping_paper_surface_area(dimensions)
-      end
+      @boxes.map { |box| wrapping_paper_surface_area(box) }.inject(:+)
+    end
 
-      total_wrapping_paper
+    def problem2
+      @boxes.map { |box| ribbon_to_wrap(box) + ribbon_for_box(box) }.inject(:+)
     end
 
     private
 
-    def wrapping_paper_surface_area(dimensions)
-      surface_area_of_sides = dimensions.combination(2).to_a.map { |x, y| x.to_i * y.to_i }
+    def dimensions(box)
+      box.split("x").map(&:to_i)
+    end
+
+    def wrapping_paper_surface_area(box)
+      surface_area_of_sides = dimensions(box).combination(2).to_a.map { |x, y| x * y }
       2 * surface_area_of_sides.inject(:+) + surface_area_of_sides.min
+    end
+
+    def ribbon_to_wrap(box)
+      2 * dimensions(box).min(2).inject(:+)
+    end
+
+    def ribbon_for_box(box)
+      dimensions(box).inject(:*)
     end
   end
 end
