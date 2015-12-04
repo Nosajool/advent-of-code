786c826c68ffc2951580ea7bed19f06aef08607f
Jason Loo <loo.jason95@gmail.com>
2015-12-04 01:11:58 -0500

    Day 3 problem 2

--- day3/day3.rb
+++ day3/day3.rb
@@ -11,23 +11,53 @@
 # > delivers presents to 2 houses: one at the starting location, and one to the east.
 # ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
 # ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
+# --- Part Two ---
+
+# The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+
+# Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+# This year, how many houses receive at least one present?
+
+# For example:
+
+# ^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+# ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+# ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+
 
 module Advent
   class Day3
 
     def initialize
       file = File.open(File.dirname(__FILE__) + "/input.txt", "r")
-      @input = file.readline
+      @moves = file.readline.split("")
     end
 
     def problem1
-      moves = @input.split("")
       current_location = [0,0]
       visited = {current_location => 1}
-      moves.each do |move|
+      @moves.each do |move|
         current_location = new_location(current_location, move)
-        visited[current_location] = visited[current_location] ? (visited[current_location] + 1) : 1
+        visited[current_location] = (visited[current_location] || 0) + 1
       end
+
+      visited.keys.count
+    end
+
+    def problem2
+      santa_location, robo_santa_location = [0,0], [0,0]
+      visited = { santa_location => 1}
+      @moves.each_with_index do |move, index|
+        if index.even?
+          santa_location = new_location(santa_location, move)
+          visited[santa_location] = (visited[santa_location] || 0) + 1
+        else
+          robo_santa_location = new_location(robo_santa_location, move)
+          visited[robo_santa_location] = (visited[robo_santa_location] || 0) + 1
+        end
+      end
+
       visited.keys.count
     end
 
